46a99e82d4b28eabdd1793af8434e0a74db15d5d
geumyoung <1gyn093@gmail.com>
2024-01-02 21:15:15 +0900

    item hover시 tooltip 표출 및 copied 문구 바꾼 후 1초 뒤 사라지게 하기

--- src/components/ResultItem.js
+++ src/components/ResultItem.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './ResultItem.module.css';
 
 const ResultItem = ({ title, symbol }) => {
-	const copyHandler = () => {
-		navigator.clipboard.writeText(symbol);
+	const [isCopied, setIsCopied] = useState(false);
+	const [isMouseOver, setIsMouseOver] = useState(false);
+
+	const hoverHandler = e => {
+		setIsMouseOver(true);
 	};
+
+	const outHandler = () => {
+		setIsMouseOver(false);
+	};
+
+	const copyHandler = e => {
+		navigator.clipboard.writeText(symbol);
+		setIsCopied(true);
+		function timmer() {
+			setIsMouseOver(false);
+		}
+		setTimeout(timmer, 1000);
+	};
+
 	return (
-		<li onClick={copyHandler} className={style.listItem}>
+		<li
+			onClick={copyHandler}
+			onMouseOver={hoverHandler}
+			onMouseOut={outHandler}
+			className={style.listItem}
+		>
+			<div className={`${style.tooltip} ${isMouseOver ? style.show : null}`}>
+				{isCopied ? 'Copied!' : 'Click to copy'}
+			</div>
 			<span>{symbol}</span>
 			{title}
 		</li>
